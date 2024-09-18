5f0b5426790ab1d7bf18aa6347ef6f75caa12c81
majkeloess <msaturczak@student.agh.edu.pl>
2024-09-18 11:47:44 +0200

    UI full implementaion

--- add.php
+++ add.php
@@ -12,7 +12,7 @@
   <link
     href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
     rel="stylesheet">
-  <title>Sati [placeholder]</title>
+  <title>Sati add</title>
 </head>
 
 <body>
@@ -33,16 +33,33 @@
             <p class="box-item-footer-text poppins-light">Fill all gaps. After clicking submit, the article will be
               loaded into the page.</p>
           </div>
-
         </div>
+        <div class="form-box">
+          <form action="db.php" method="post">
+            <div class="form-row">
 
+              <label class="form-label poppins-medium" for="title">Title</label>
+              <input class="input-smaller poppins-regular" type="text" id="title" name="title" required>
+            </div>
+            <div class="form-row">
+              <label class="form-label poppins-medium" for="type">Topic</label>
+              <input class="input-smaller poppins-regular" type="text" id="type" name="type" required>
+            </div>
+            <div class="form-row">
+              <label class="form-label poppins-medium" for="content">Content</label>
+              <textarea class="input-larger poppins-regular" type="content" id="content" name="content"
+                required></textarea>
+            </div>
+            <button class="form-button poppins-semibold" type="submit" value="Wyślij">Submit</button>
+
+          </form>
+        </div>
       </div>
 
 
     </div>
   </div>
-  <footer class="footer poppins-regular">©2024 <a target="_blank" class="footer-a"
-      href="https://www.majkeloess.dev/">majkeloess</a>
+  <footer class="footer-add poppins-regular">©2024 <a target="_blank" href="https://www.majkeloess.dev/">majkeloess</a>
   </footer>
 </body>
 
